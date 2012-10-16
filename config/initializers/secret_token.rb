481c0766e8e9dcb19d2dcc2e35516b68277d0ed0
Naveed Ehmad <naveed.ehmad@gmail.com>
2012-10-16 22:34:36 +0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sitegen::Application.config.secret_token = '2237e6aacbcb587840f532b73315356d17a985583ea9b519d59a6b207cabdfc9089f1ace5e399400f4d656cae96b63ed98e7adec373fa4008dabf2ba7a27d22d'
